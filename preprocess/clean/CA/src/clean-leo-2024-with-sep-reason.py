a71b4295ab524603da4e1bd01aeb54971b161911
Tarak Shah <tarak@hrdag.org>
2025-10-14 16:16:15 -0700

    for ca leo data, use simpler rules to split names, see #60

--- preprocess/clean/CA/src/clean-leo-2024-with-sep-reason.py
+++ preprocess/clean/CA/src/clean-leo-2024-with-sep-reason.py
@@ -22,17 +22,28 @@ term_code_dictionary = {
 
 def getargs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input")
+    parser.add_argument("--input", default = "input/leo-2024-with-sep-reason.xlsx")
     parser.add_argument("--output")
     return parser.parse_args()
 
 
+def cleanstring(strings):
+    return (
+        strings.fillna("")
+        .str.replace(r"\s+", " ", regex=True)
+        .str.strip()
+        .str.upper()
+    )
+
+
 def clean_names(df):
     out = df
-    hn = out.officer_name.apply(nameparser.HumanName)
-    out["last_name"] = hn.apply(lambda x: x.last)
-    out["first_name"] = hn.apply(lambda x: x.first)
-    out["middle_name"] = hn.apply(lambda x: x.middle)
+    split_names = out.officer_name.str.split(",", expand=True)
+    out["last_name"] = cleanstring(split_names[0])
+    first_middle_sfx = cleanstring(split_names[1].fillna(""))
+    out["first_name"] = first_middle_sfx.str.extract(r"^([^\s]+)")
+    middle_sfx = first_middle_sfx.str.extract(r"^[^\s]+\s+(.*)$")[0]
+    out["middle_name"] = middle_sfx.str.replace(r"\s+(JR|SR|II|III|IV|JUNIOR|JR\.)$", "", regex=True).str.strip()
     return out
 
 
